4ba29ccb5f36a234eda96757aab5f9337671db68
Kannapi <112032889+itsKannapi@users.noreply.github.com>
2022-12-19 13:47:14 -0600

    Update logicfile.js

--- logicfile.js
+++ logicfile.js
@@ -1,6 +1,8 @@
-var songNameArray = ["Rei", "Running in the 90's", "Embraced by the Flame", ""];
-var songArtistArray = ["The Oral Cigarettes"];
-var songMP3FileArray = ["osuRei.mp3"];
+//to do:
+// create the songs array 
+var songNameArray = ["Rei", "Running in the 90's", "Embraced by the Flame", "Brave Climber", "Harumachi Clover", "Cycle Hit", "SPUTNIK-3", "Ray", "Loser Girl", "Exit this Earth's Atmosphere"];
+var songArtistArray = ["The Oral Cigarettes", "Max Coveri", "Imperial Circus Decadence", "Minase Inori", "Will Stetson Cover", "Kasai Hardcores", "II-L", "Passcode", "Tuyu", "Camellia"];
+var songMP3FileArray = ["osuRei.mp3","osuRunning90s.mp3","osuEmbraced.mp3"];
 
 function songChanger(){
     
