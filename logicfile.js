102405c924453424c7b8bdca06842e4b716d7c7f
Kannapi <112032889+itsKannapi@users.noreply.github.com>
2022-12-19 12:38:23 -0600

    began logic code for the osuGame
    to do:
    complete song arrays
    complete html file containing songs

---
+++ logicfile.js
@@ -0,0 +1,8 @@
+var songNameArray = ["Rei", "Running in the 90's", "Embraced by the Flame", ""];
+var songArtistArray = ["The Oral Cigarettes"];
+var songMP3FileArray = ["osuRei.mp3"];
+
+function songChanger(){
+    
+}
+
